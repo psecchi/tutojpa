b774e94ac5c02f518d5b915686b3a8a72e064115
psecchi <p.secchi@proxiad.com>
2018-02-13 16:31:12 +0100

    rename table

--- formation-jpa/src/main/java/com/proxiad/formation/jpa/model/Transaction.java
+++ formation-jpa/src/main/java/com/proxiad/formation/jpa/model/Transaction.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "transaction")
+@Table(name = "transaction_bancaire")
 @DiscriminatorColumn(name="type")
 public abstract class Transaction {
 
